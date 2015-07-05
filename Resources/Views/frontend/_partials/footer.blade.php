020d8ea8631127a7a1092d1878e8b4c44e7dea23
illuminate3 <carsten.sapporo@gmail.com>
2015-07-05 14:39:15 -0500

    cms 8

--- Resources/Views/frontend/_partials/footer.blade.php
+++ Resources/Views/frontend/_partials/footer.blade.php
@@ -14,19 +14,11 @@
 					{{ Config::get('general.footer') }}
 				</p>
 			</div>
-			<div class="col-sm-4">
-
+			<div class="col-sm-2">
 				{!! Plugin::Featured() !!}
-
-{{--
-				<ul class="pull-right">
-					<li>{!! link_to_route('dashboard', 'Home') !!}</li>
-					<li>{!! link_to_route('dashboard.faq', 'Faq') !!}</li>
-					<li>{!! link_to_route('dashboard.contact', 'Contact Us') !!}</li>
-					<li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li>
-					<!--#gototop-->
-				</ul>
---}}
+			</div>
+			<div class="col-sm-2">
+				{!! Plugin::Timed() !!}
 			</div>
 		</div>
 
